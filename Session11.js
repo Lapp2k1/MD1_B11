44db9d8ff35cc19673bdb182b430bc0a8d2a9b4f
Lapp2k1 <vulengocvip2015@gmail.com>
2024-04-24 14:56:45 +0700

    first commit

---
+++ Session11.js
@@ -0,0 +1,23 @@
+let browser = "";
+
+switch (browser) {
+  case "IE":
+    console.log("Not support!");
+    break;
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Edge":
+    console.log("Support!");
+    break;
+  default:
+    console.log("Unknown browser!");
+}
+	
+var a = (20 - (20 % 3))/3;
+console.log(a);
+
+let b=4
+let c=5
+alert(b+c);
+
